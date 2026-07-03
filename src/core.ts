f2e9c7dc8112f01269196e030e6fcb982004cbf8
cevheri <cevheribozoglan@gmail.com>
2026-07-03 10:37:34 +0300

    chore: release 0.2.0

--- src/core.ts
+++ src/core.ts
@@ -22,7 +22,7 @@
  */
 
 /** The LibreDB package version. Kept in sync with package.json. */
-export const version = "0.1.3";
+export const version = "0.2.0";
 
 /**
  * The stable failure codes of the kernel. Every error the kernel throws is a
